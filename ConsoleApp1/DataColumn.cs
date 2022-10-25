c890ad7a2b3690e4f6969d799741a399143ec0f6
Александр Иванович Самаркин <alexsamarkinru@gmail.com>
2022-10-26 02:57:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/DataColumn.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// колонка данных
+    /// </summary>
+    public class DataColumn
+    {
+        /// <summary>
+        /// имя колонки 
+        /// </summary>
+        public string Name { get; set; } = "Data1";
+
+        /// <summary>
+        /// данные в колонке (реализованы в виде списка десятичных дробей)
+        /// на этом этапе создается лист, в котором нет данных
+        /// </summary>
+        public List<double> Items { get; set; } = new List<double>();
+
+        /// <summary>
+        /// добавление переменной
+        /// </summary>
+        /// <param name="d">переменная, которая добавляется к списку</param>
+        public void Add(double d) => Items.Add(d);
+
+        /// <summary>
+        /// заполнить интервал от start до stop с шагом step
+        /// </summary>
+        /// <param name="start">начальное значение (включительно)</param>
+        /// <param name="step">шаг</param>
+        /// <param name="stop">конечное значение (не включая)</param>
+        public void Arange(double start, double step, double stop)
+        {
+            Items.Clear();
+            double d = start;
+            while (d<stop)
+            {
+                Items.Add(d);
+                d += step;
+            }
+        }
+
+        public void Linspace(double start, int N, double stop)
+        {
+            double step = (stop-start) / N;
+            Arange(start,step,stop);
+        }
+
+        public void Seq(double start, double step, int N)
+        {
+            Items.Clear();
+            for (int i = 0; i < N; i++)
+            {
+                Items.Add((double)i*step+start);
+            }
+        }
+
+        public void Fill(double d, int N) => Seq(d, 0, N);
+        public void Zeros(int N) => Fill(0, N);
+        public void Ones(int N) => Fill(1, N);
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Name);
+            foreach (double d in Items)
+            {
+                sb.AppendLine($"{d}");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataColumn dc = new DataColumn();
+            dc.Seq(0,0.1,500);
+            // output and waiting for Enter key
+            Console.WriteLine(dc.ToString());
+            Console.ReadLine();
+        }
+    }
+}
